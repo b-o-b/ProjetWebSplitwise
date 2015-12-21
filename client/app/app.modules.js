218b9fc1ceea472a7e3d6b1e9aeb45fc2c822ec1
abdessamad essaydi <abdessaydi@gmail.com>
2015-12-21 21:31:22 +0100

    création du route de login

--- client/app/app.modules.js
+++ client/app/app.modules.js
@@ -2,6 +2,6 @@
 	'use strict';
 
 	angular
-		.module('appModule', [])
+		.module('appModule', ['ui-rout'])
 
 })();
--- client/app/app.routes.js
+++ client/app/app.routes.js
@@ -1,1 +1,19 @@
 // ROUTES FILE
+(function(){
+	'use strict'
+
+	 angular
+		.module('appModule')
+		.config(['$stateProvide', '$urlRouterProvider', function($stateProvide,$urlRouterProvider){
+		.state('login', {
+      url: "/",
+      templateUrl: 'app/components/singnupView.html'
+      controller: 'signupController'
+
+    })	
+		})
+
+    
+
+
+})();
